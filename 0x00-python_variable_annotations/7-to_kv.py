c8d0c40af1903764347537faa365a7a8844357fb
BcodezMUJJE <billbrightmatege@gmail.com>
2024-01-11 17:28:34 +0000

    to_kv

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,16 +1,13 @@
 #!/usr/bin/env python3
 
-'''amodule that takes two arguments and returns a tuple'''
 
+'''Task 7: Complex types - string and int/float to tuple
+'''
+from typing import Union, Tuple
 
-from typing import Tuple, Union
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
-    '''A function that retuns a tuple'''
-    return(k, v**2)
-
-
-if __name__ == '__main__':
-    print(to_kv.__annotations__)
-    print(to_kv("eggs", 3))
-    print(to_kv("school", 0.02))
+    '''Converts a key and its value to a tuple of the key and
+    the square of its value.
+    '''
+    return (k, float(v**2))
