267732f54b3c840db614912eb94eee9cc5c3122c
BcodezMUJJE <billbrightmatege@gmail.com>
2024-01-11 17:16:22 +0000

    concat

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,16 +1,11 @@
 #!/usr/bin/env python3
 
-'''Amodule that concatenates two strings'''
+
+'''Task 1: Basic annotations - concat
+'''
 
 
 def concat(str1: str, str2: str) -> str:
-    '''A function that concatenates 2 strings'''
+    '''Concatenates two strings.
+    '''
     return str1 + str2
-
-if __name__ == '__main__':
-
-    str1 = 'sammy'
-    str2 = 'kingx'
-
-    print(concat(str1, str2) == "{}{}".format(str1, str2))
-    print(concat.__annotations__)
