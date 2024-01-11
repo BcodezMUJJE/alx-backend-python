c0bffc9624b86f9b909c26e1e82dc781590112a5
BcodezMUJJE <billbrightmatege@gmail.com>
2024-01-11 17:30:42 +0000

    length

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,15 +1,14 @@
 #!/usr/bin/env python3
 
-'''Annotating the below function’s parameters and return
-values with the appropriate types
+
+'''Task 9: Let's duck type an iterable object
 '''
 
-from typing import Sequence, Iterable, List, Tuple
+
+from typing import Iterable, List, Sequence, Tuple
+
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
-    '''Annotating the function parameters'''
+    '''Computes the length of a list of sequences.
+    '''
     return [(i, len(i)) for i in lst]
-
-
-if __name__ == '__main__':
-    print(element_length.__annotations__)
