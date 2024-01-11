afad67bcd22e32cf7b686ef8ab9471b1df7bb7f6
BcodezMUJJE <billbrightmatege@gmail.com>
2024-01-11 10:58:58 +0000

    sum_list

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+'''Amodule for returning the sum of a list'''
+
+
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    '''Afunction thst returns the sum of a list'''
+
+    total = 0.0
+    for i in input_list:
+        total += i
+    return total
+
+
+if __name__ == '__main__':
+    floats = [3.14, 1.11, 2.22]
+    floats_sum = sum_list(floats)
+    print(floats_sum == sum(floats))
+    print(sum_list.__annotations__)
+    print("sum_list(floats) returns {} which is a {}"
+          .format(floats_sum, type(floats_sum)))
