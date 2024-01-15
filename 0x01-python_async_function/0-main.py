1d766b8a5751ee31c1e8cf4b5fe82defcab067f6
BcodezMUJJE <billbrightmatege@gmail.com>
2024-01-15 11:12:45 +0000

    basic_async_syntax

--- 0x01-python_async_function/0-main.py
+++ 0x01-python_async_function/0-main.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python3
 
 import asyncio
+from 0-basic_async_syntax.py import wait_random
 
-wait_random = __import__('0-basic_async_syntax').wait_random
+async def main():
+    print(await wait_random())
+    print(await wait_random(5))
+    print(await wait_random(15))
 
-print(asyncio.run(wait_random()))
-print(asyncio.run(wait_random(5)))
-print(asyncio.run(wait_random(15)))
+# Create an event loop
+loop = asyncio.get_event_loop()
+
+# Run the main coroutine
+loop.run_until_complete(main())
